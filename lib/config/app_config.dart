a28e73cfed68add948c25d7f7a0c4447e09d286f
Hoooooou-Jun <jipkim2@gmail.com>
2025-06-12 15:04:58 +0900

    [Chore, fix] 의존성 업데이트, config 파일 수정

--- lib/config/app_config.dart
+++ lib/config/app_config.dart
@@ -1,13 +1,21 @@
+import 'dart:io' show Platform;
 import 'package:flutter/foundation.dart';
 
 class AppConfig {
-  /// 로컬 서버
-  static const apiLocalUrl = 'http://localhost:3000';
+  /// 로컬 서버 (플랫폼별 호스트)
+  static String get apiLocalUrl {
+    if (kReleaseMode) {
+      return apiProductionUrl;
+    }
+    if (Platform.isAndroid) {
+      return 'http://10.0.2.2:3000';
+    }
+    return 'http://localhost:3000';
+  }
 
   /// 프로덕션 서버
   static const apiProductionUrl = 'http://121.143.179.182:8081';
 
-  static String get apiBaseUrl {
-    return kReleaseMode ? apiProductionUrl : apiLocalUrl;
-  }
+  /// 실제로 쓰일 베이스 URL
+  static String get apiBaseUrl => apiLocalUrl;
 }
