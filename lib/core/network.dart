bcd50112fc5451d43fd79bf27170555f44073852
Hoooooou-Jun <jipkim2@gmail.com>
2025-06-16 20:19:59 +0900

    [feat, refactor] API 요청 인터셉터 삽입, 불필요한 타입 검증 제거

--- lib/core/network.dart
+++ lib/core/network.dart
@@ -1,10 +1,63 @@
 import 'package:dio/dio.dart';
 import '../config/app_config.dart';
+import 'secure_storage.dart';
 
 class Network {
-  static final Dio dio = Dio(
-    BaseOptions(
+  static final Dio dio = _createDio();
+
+  static Dio _createDio() {
+    final dio = Dio(BaseOptions(
       baseUrl: AppConfig.apiBaseUrl,
-    ),
-  );
+      connectTimeout: const Duration(milliseconds: 5000),
+      receiveTimeout: const Duration(milliseconds: 3000),
+    ));
+
+    dio.interceptors.add(
+      InterceptorsWrapper(
+        onRequest: (options, handler) async {
+          final token = await SecureStorage.read(key: 'access_token');
+          if (token != null && token.isNotEmpty) {
+            options.headers['Authorization'] = 'Bearer $token';
+          }
+          return handler.next(options);
+        },
+        onError: (err, handler) async {
+          final response = err.response;
+          final requestOptions = err.requestOptions;
+          if (response?.statusCode == 401 && requestOptions.extra['retried'] != true) {
+            try {
+              final refreshToken = await SecureStorage.read(key: 'refresh-token');
+              final accessToken  = await SecureStorage.read(key: 'access-token');
+
+              final response = await dio.post(
+                '/api/user/refresh',
+                data: {
+                  'accessToken': accessToken,
+                  'refreshToken': refreshToken,
+                },
+              );
+
+              final newAccess = response.data['accessToken'] as String;
+
+              await SecureStorage.write(key: 'access-token', value: newAccess);
+
+              requestOptions.headers['Authorization'] = 'Bearer $newAccess';
+
+              requestOptions.extra['retried'] = true;
+
+              final cloned = await dio.fetch(requestOptions);
+              return handler.resolve(cloned);
+            } catch (e) {
+              /** 로그인 만료 알림 떠야함. */
+              return handler.next(err);
+            }
+          }
+
+          return handler.next(err);
+        },
+      ),
+    );
+
+    return dio;
+  }
 }
