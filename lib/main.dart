38eb91d93a6fff92d8be313c26f2259f529a3c28
Gusq1s <phyunbinp@gmail.com>
2025-06-17 19:02:40 +0900

    [Feat] firebase initialization 구현

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
+import 'package:cba_connect_application/firebaseService/initialize_firebase.dart';
 import 'package:cba_connect_application/presentation/login/login_view.dart';
 import 'package:cba_connect_application/presentation/main/main_view.dart';
 import 'package:flutter/material.dart';
-// import 'package:cba_connect_application/presentation/login/login_view.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:cba_connect_application/presentation/main/pages/home/carpool_search_view.dart';
+
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -35,6 +36,13 @@ void main() async {
   /**
    * 해당 부분에 권한 요청 및 기타 API 처리, 혹은 버전 및 업데이트 확인 코드 넣으면 됨.
    */
+  await initializeFirebaseAppSettings();
+
+
+
+
   runApp(ProviderScope(child: MyApp()));
   FlutterNativeSplash.remove();
+
+
 }
