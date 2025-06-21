b9b93aa13f227f627836725a4d3710c9ba94f4e7
Gusq1s <phyunbinp@gmail.com>
2025-06-21 12:31:59 +0900

    [Feat] IOS FCM 토큰 발급 과정

--- lib/dto/refresh_fcm_token_dto.dart
+++ lib/dto/refresh_fcm_token_dto.dart
@@ -2,16 +2,19 @@ class RefreshFcmTokenDto {
   final int userId;
   final String oldToken;
   final String newToken;
+  final String platform;
 
   RefreshFcmTokenDto({
     required this.userId,
     required this.oldToken,
     required this.newToken,
+    required this.platform,
   });
 
   Map<String, dynamic> toJson() => {
     'userId': userId,
     'oldToken': oldToken,
     'newToken': newToken,
+    'platform': platform,
   };
 }
--- lib/dto/regist_fcm_dto.dart
+++ lib/dto/regist_fcm_dto.dart
@@ -1,14 +1,17 @@
 class RegistFcmDto {
   final int userId;
   final String token;
+  final String platform;
 
   RegistFcmDto({
     required this.userId,
     required this.token,
+    required this.platform
   });
 
   Map<String, dynamic> toJson() => {
     'userId': userId,
     'token': token,
+    'platform': platform,
   };
 }
